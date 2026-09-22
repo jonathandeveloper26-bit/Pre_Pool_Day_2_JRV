c397c95a59ccd6cc2de2a5a1a44bb96b98415495
jonathandeveloper26-bit <jonathan.developer26@gmail.com>
2026-09-22 11:58:45 +0200

    challenge1.py Using Math Libarary: .lcm

---
+++ challenge1.py
@@ -0,0 +1,17 @@
+import math
+
+
+r1 = range(1,5)
+r2 = range(1,8)
+
+print(math.lcm(*r1))
+print(math.lcm(*r2))
+
+
+r3 = range(1,21)
+r4 = range(1,201)
+r5 = range(1,2001)
+
+print(math.lcm(*r3))
+print(math.lcm(*r4))
+print(math.lcm(*r5))
